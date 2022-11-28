dbf48fed1c88b27a5c5194f5f726ae1a2ae5ee24
atronee <otavio.oam@protonmail.com>
2022-11-28 18:54:53 -0300

    adicione a função de scalling para os sprites

--- Polidroids_demo/utils.py
+++ Polidroids_demo/utils.py
@@ -1,11 +1,12 @@
 import random # Importa a biblioteca random
 from pygame.image import load # Importa o método load do módulo image do pygame
 from pygame.math import Vector2 # Importa o módulo math do pygame
+import pygame
 
-def load_sprite(name, with_alpha=True): # Método carrega uma imagem
+def load_sprite(name, scale, with_alpha=True): # Método carrega uma imagem
     path = f"Assets/Sprites/{name}.png" # Define o caminho da imagem
     loaded_sprite = load(path) # Carrega a imagem
-
+    loaded_sprite = pygame.transform.scale(loaded_sprite, (loaded_sprite.get_size()[0]*scale, loaded_sprite.get_size()[1]*scale)) # Define o tamanho da imagem
     if with_alpha:
         return loaded_sprite.convert_alpha() # Retorna a imagem convertida com alpha
     else: 
