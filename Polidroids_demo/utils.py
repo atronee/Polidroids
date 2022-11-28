59f621336b51dfb193411e9ea2e5b599a2591337
GabrielJP314 <gabrielpereirajp1201@gmail.com>
2022-11-28 10:55:57 -0300

    Adicione a classe Spaceship.

--- Polidroids_demo/utils.py
+++ Polidroids_demo/utils.py
@@ -1,4 +1,5 @@
 from pygame.image import load # Importa o método load do módulo image do pygame
+from pygame.math import Vector2 # Importa o módulo math do pygame
 
 def load_sprite(name, with_alpha=True): # Método carrega uma imagem
     path = f"Assets/Sprites/{name}.png" # Define o caminho da imagem
@@ -8,3 +9,8 @@ def load_sprite(name, with_alpha=True): # Método carrega uma imagem
         return loaded_sprite.convert_alpha() # Retorna a imagem convertida com alpha
     else: 
         return loaded_sprite.convert() # Retorna a imagem convertida
+    
+def wrap_position(position, surface): # Método retorna a posição do objeto dentro da tela
+    x, y = position # Desempacota a posição
+    w, h = surface.get_size() # Calcula o tamanho da tela
+    return Vector2(x % w, y % h) # Retorna a posição dentro da tela
